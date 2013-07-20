f76d6fe5309d1af3eb8de46010b60d4371d7bfe3
Lastangel <kingpin2086@fullsail.edu>
2013-07-20 00:00:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,45 @@
 // Arthor: Clifton Abraham
 // VFW 1307
 // Project 2 07/16/2013
+window.addEventListener("DOMContentLoaded", function(){
+var id;
+var tasks;
+// getElements
+ function $(x){
+var information = document.getElementById(x);
+    return information;
+   }
+   // set Links
+  /* var displayLink = $("displayLink");
+   displayLink.addEventListener("click", getData);*/
+   
+ /*  var clearData = $("clear");
+   clearData.addEventListener("click", clearStorage);*/
+   
+   var saveData = $("complete");
+   saveData.addEventListener("click", saveData);
+   
+        saveData = function(){
+        var id = math.floor( math.random() * 1000000001 );
+        tasks = {};
+        tasks = ["First name:", $("fname").value ];
+        tasks = ["Last Name:" , $("lname").value  ];
+        tasks = ["Email:" , $("email").value ];
+        tasks = ["Password:" , $("pword").value ];
+        tasks = ["Comments:", $("comments").value];
+        }
+      
+        // Saved Data
+     localStorage.setItem(id , JSON.stringify(tasks));
+     alert("Tasks Saved");
 
-// getElements 
+/*
 var fName = document.getElementById("fname");
 var lName = document.getElementById("lname");
 var pWord = document.getElementById("pword");
 var eMail = document.getElementById("email");
-var checkBoxes = document.getElementById("myForm").chor
-var confirmTasks = document.getElementById("confirm");
+//var checkBoxes = document.getElementById("myForm").chor
+//var confirmTasks = document.getElementById("complete");
 //var myTask = document.getElementbyName("ul")
 // checkboxes function
 //console.log(checkBoxes);
@@ -22,6 +53,11 @@ var confirmTasks = document.getElementById("confirm");
     };
 
 };
+ //for loop on localStorage
+  for(i=0, j=localStorage.length; i<j; i++){
+ console.log(localStorage.key(i));
+ }
+ 
 
 //localStorage function
  var collectData = function(){
@@ -30,18 +66,32 @@ var confirmTasks = document.getElementById("confirm");
     var pWord = localStorage.setItem("PassWord" , pword.value);
     var eMail = localStorage.setItem("Email"    , email.value);
    // var myTask = localStarage.setItem("ul", ul.value);
+   // Store Data function
+   var saveData = function(){
+        var id = math.floor( math.random() * 1000000001 );
+        tasks = {};
+        tasks = ["First name:", $("fname").value ];
+        tasks = ["Last Name:" , $("lname").value  ];
+        tasks = ["Email:" , $("email").value ];
+        tasks = ["Password:" , $("pword").value ];
+        tasks = ["Comments:", $("comments").value];
+       // tasks = ["My Tasks:"$("myTasks").value]
+       
+ // Saved Data
+ localStorage.setItem(id , JSON.stringify(tasks));
+ alert("Tasks Saved");
+    
+ };
 };
 // eventlisteners
  fname.addEventListener("blur", collectData);
  lname.addEventListener("blur", collectData);
  pWord.addEventListener("blur", collectData);
  eMail.addEventListener("blur", collectData);
- confirmTasks.addEventListener("click",checkValue);
+ confirmTasks.addEventListener("click", checkValue);
  //myTask.addEventListener("blur",collectData);
+*/
 
- //for loop on localStorage
- for(i=0, j=localStorage.length; i<j; i++){
- console.log(localStorage.key(i));
- }
- 
- 
+
+}); // end of function 
+
